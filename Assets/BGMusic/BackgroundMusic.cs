ac463ee66ef91074da6bf3eb44f7a0600405bde6
Hilary R <royh11@uqat.ca>
2024-12-12 14:20:07 -0500

    modification bg music
    moins fort, success

--- Assets/BGMusic/BackgroundMusic.cs
+++ Assets/BGMusic/BackgroundMusic.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BackgroundMusic : MonoBehaviour
 {
     public AudioSource audioSource;  // L'AudioSource qui va jouer la musique
-    public AudioClip minstrel2;      // Musique de fond (Minstrel2)
+    public AudioClip Minstrel1;      // Musique de fond (Minstrel2)
 
     void Start()
     {
@@ -11,8 +11,8 @@ public class BackgroundMusic : MonoBehaviour
         audioSource.loop = true;
 
         // Charge et joue la musique de fond Minstrel2
-        audioSource.clip = minstrel2;
-        audioSource.volume = 0.1f; // Volume faible pour être subtile
+        audioSource.clip = Minstrel1;
+        audioSource.volume = 0.2f; // Volume faible pour être subtile
         audioSource.Play();
     }
 }
